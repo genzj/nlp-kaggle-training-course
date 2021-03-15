e89b1822b33472d97d10791507b21bcf62e0f988
genzj <zj0512@gmail.com>
2021-03-14 22:36:50 -0700

    porting bert to TF 2.4.1

--- bert/bert/modeling.py
+++ bert/bert/modeling.py
@@ -25,7 +25,7 @@ import math
 import re
 import numpy as np
 import six
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 class BertConfig(object):
@@ -361,8 +361,9 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return tf.contrib.layers.layer_norm(
-      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+  return tf.keras.layers.LayerNormalization(
+    name=name, axis=-1, epsilon=1e-12, dtype=tf.float32
+  )(input_tensor)
 
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
